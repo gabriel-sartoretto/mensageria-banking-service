f14b426c7a31d436a37fe76ff316d4e652b66f3b
Gabriel Sartoretto <gabrielsartoretto@gmail.com>
2026-09-24 18:38:33 -0300

    adicionando o AVRO

--- src/main/java/br/com/alura/service/messaging/RemoverAgenciaService.java
+++ src/main/java/br/com/alura/service/messaging/RemoverAgenciaService.java
@@ -1,5 +1,6 @@
 package br.com.alura.service.messaging;
 
+import br.com.alura.Agencia;
 import br.com.alura.domain.messaging.AgenciaMensagem;
 import br.com.alura.service.http.AgenciaService;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -12,18 +13,29 @@ import org.eclipse.microprofile.reactive.messaging.Incoming;
 public class RemoverAgenciaService {
 
     private final AgenciaService agenciaService;
-    private final ObjectMapper objectMapper;
+    //private final ObjectMapper objectMapper;
 
-    public RemoverAgenciaService(AgenciaService agenciaService, ObjectMapper objectMapper) {
+    public RemoverAgenciaService(
+            AgenciaService agenciaService
+            //ObjectMapper objectMapper
+    ) {
         this.agenciaService = agenciaService;
-        this.objectMapper = new ObjectMapper();
+        //this.objectMapper = new ObjectMapper();
     }
 
     @WithTransaction
     @Incoming("remover-agencia-channel")
-    public Uni<Void> consumirMensagem(String mensagem) {
+    public Uni<Void> consumirMensagem(Agencia mensagem) {
         try{
-            AgenciaMensagem agenciaMensagem = objectMapper.readValue(mensagem, AgenciaMensagem.class);
+            //Usar o ObjectMapper quando for Json
+            //AgenciaMensagem agenciaMensagem = objectMapper.readValue(mensagem, AgenciaMensagem.class);
+            AgenciaMensagem agenciaMensagem = new AgenciaMensagem(
+                    1,
+                    mensagem.getNome(),
+                    mensagem.getRazaoSocial(),
+                    mensagem.getCnpj(),
+                    mensagem.getSituacaoCadastral()
+            );
             return agenciaService.buscarPorCnpj(agenciaMensagem.getCnpj())
                     .onItem().ifNotNull().transformToUni(agencia ->
                        agenciaService.deletar(agencia.getId())
